c191dfcec20c47ca3347a4c3a7ef4c644e899d1d
aimanrafee <39325438+aimanrafee@users.noreply.github.com>
2026-01-23 10:59:11 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'nukleus-core-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('NUKLEUS: Seeding Core Cache');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
